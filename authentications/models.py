6964cec91a34ea301c2349fabee62a353f9c20ca
Awal Islam <awal.aaraf92@gmail.com>
2026-01-29 13:26:01 +0600

    add CustomUserManager to authentications app

--- authentications/models.py
+++ authentications/models.py
@@ -1,7 +1,32 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser,BaseUserManager
+from django.contrib.auth.models import BaseUserManager
 
-class
+class CustomUserManager(BaseUserManager):
+
+    def create_user(self,email,password=None,**extra_fields):
+        if not email:
+            raise ValueError('The email field must be send')
+        
+        email=self.normalize_email(email=email)
+        user=self.model(email=email,**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+    
+    def create_superuser(self,email,password=None,**extra_fields):
+        extra_fields.setdefault('is_staff',True)
+        extra_fields.setdefault('is_superuser',True)
+        
+        if not extra_fields.get('is_staff'):
+            raise ValueError('Super_user must have is_staff=True')
+        
+        if not extra_fields.get('is_superuser'):
+            raise ValueError('Super_user must have is_superuser=True')
+        return self.create_user(email=email,password=password,**extra_fields)
+
+
 
 class CustomUsers(AbstractUser):
     username=None
@@ -13,3 +38,7 @@ class CustomUsers(AbstractUser):
     USERNAME_FIELD='email'
     REQUIRED_FIELDS=[]
 
+    objects=CustomUserManager()
+
+    def __str__(self):
+        return self.email
