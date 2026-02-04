70a6fdde02d26e73c1ac30987a13923c2ab70ae8
Awal Islam <awal.aaraf92@gmail.com>
2026-02-04 23:19:00 +0600

    fix: resolve bug in product URLs

--- products/products_urls.py
+++ products/products_urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns=[
     # path('',views.api_products,name='products-list'),
-    path('',views.ViewProducts),
+    path('',views.ViewProducts.as_view()),
     # path('/<int:pk>',views.view_specific_product),
     path('/<int:pk>',views.ViewProduct.as_view())
 
