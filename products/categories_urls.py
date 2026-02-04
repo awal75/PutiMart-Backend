78d8afc0b9067a13e318ab4ca1d25a0e26baa71b
Awal Islam <awal.aaraf92@gmail.com>
2026-02-04 23:32:14 +0600

    refactor: convert products API from FBV to CBV

--- products/categories_urls.py
+++ products/categories_urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.api_categories ,name='api-categories'),
+    # path('',views.api_categories ,name='api-categories'),
+    path('',views.ViewCategories.as_view() ,name='api-categories'),
     # path('/<int:pk>',views.view_specific_category,name='view-specfic-category')
     path('/<int:pk>',views.ViewCategory.as_view())
 ]
--- products/views.py
+++ products/views.py
@@ -19,6 +19,18 @@ def api_categories(request):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data,status=status.HTTP_201_CREATED)
+    
+class ViewCategories(APIView):
+    def get(self,request):
+        categories=Category.objects.all()
+        serializer=CategorySerializer(categories,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    
+    def post(self,request):
+        serializer=CategorySerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data,status=status.HTTP_201_CREATED)
 
 
 @api_view(['GET','PUT','DELETE','PATCH'])
