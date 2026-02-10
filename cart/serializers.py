4f8eb1c1248bec75d830f07ff90a260a175bcaff
Awal Islam <awal.aaraf92@gmail.com>
2026-02-10 23:51:12 +0600

    refactor(cart): update cart and cartitem serializer

--- cart/serializers.py
+++ cart/serializers.py
@@ -1,23 +1,37 @@
 from rest_framework import serializers
 from .models import Cart,CartItem
+from products.models import Product
 
 
 
 
 
+class SimpleProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Product
+        fields=['id','name','price']
 
 
 class CartItemSerializer(serializers.ModelSerializer):
+    product=SimpleProductSerializer()
+    total_price=serializers.SerializerMethodField(method_name='get_total_price')
     class Meta:
         model=CartItem
-        fields=['id','cart','product','quantity']
+        fields=['id','product','quantity','total_price']
+
+    def get_total_price(self,cartItem: CartItem):
+        return cartItem.quantity*cartItem.product.price
 
 
 class CartSerializer(serializers.ModelSerializer):
     cartitems=CartItemSerializer(many=True)
+    total_price=serializers.SerializerMethodField(method_name='get_total_price')
 
     class Meta:
         model=Cart
-        fields=['id','user','created_at','cartitems']
+        fields=['id','user','total_price','created_at','cartitems']
+
+    def get_total_price(self,cart: Cart):
+        return sum([item.product.price * item.quantity for item in cart.cartitems.all()])
 
     
