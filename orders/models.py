f13f5853f8a3e3cc619a151b80313bb918916f06
Awal Islam <awal.aaraf92@gmail.com>
2026-02-20 17:54:21 +0600

    updare order model and orderItem modl

--- orders/models.py
+++ orders/models.py
@@ -6,15 +6,20 @@ User=get_user_model()
 
 class Order(models.Model):
     CHOICES=[
-        ('pending','Pending'),
+        ('not paid','Not Paid'),
+        ('ready to ship','Ready To Ship'),
         ('shipped','Shipped'),
-        ('delivered','Delivered')
+        ('delivered','Delivered'),
+        ('canceled','Canceled')
     ]
     user=models.ForeignKey(User,on_delete=models.CASCADE,related_name='orders')
-    status=models.CharField(max_length=25,choices=CHOICES,default='pending')
+    status=models.CharField(max_length=25,choices=CHOICES,default='not paid')
     total_price=models.DecimalField(max_digits=12,decimal_places=2)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Order {self.id} by {self.username} - {self.status}'
 
 
 
@@ -23,3 +28,7 @@ class OrderItem(models.Model):
     product=models.ForeignKey(Product,on_delete=models.CASCADE,related_name='orderitems')
     quantity=models.PositiveIntegerField(default=0)
     price=models.DecimalField(max_digits=12,decimal_places=2)
+    total_price=models.DecimalField(max_digits=12,decimal_places=2)
+
+    def __str__(self):
+        return f'{self.quantity} x {self.product.name}'
